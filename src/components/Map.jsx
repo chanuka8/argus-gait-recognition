7d48093fc67de5e6040b8672487427728c9a47bd
Dilshanrawishka <drawishka@gmail.com>
2026-05-04 23:46:43 +0530

    Resize map container, Restrict map to Sri Lanka Bounds

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,12 +4,25 @@ import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 const Map = () => {
-    // Default position (London)
-    const position = [51.505, -0.09];
+    // Default position (Sri Lanka)
+    const position = [7.8731, 80.7718];
+    
+    // Restrict map to Sri Lanka bounds [SouthWest, NorthEast]
+    const sriLankaBounds = [
+        [5.8, 79.5],
+        [9.9, 82.0]
+    ];
 
     return (
         <div className="map-container">
-            <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+            <MapContainer 
+                center={position} 
+                zoom={7} 
+                minZoom={7}
+                maxBounds={sriLankaBounds}
+                maxBoundsViscosity={1.0}
+                scrollWheelZoom={true}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
