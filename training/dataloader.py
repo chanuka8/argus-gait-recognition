24fe0dba41dd03d44875098b6d3e450948f54075
Chanuka Sandun <chanuka2000sandun@gmail.com>
2026-07-22 04:29:11 +0530

    Update gait model final performance metrics report

--- training/dataloader.py
+++ training/dataloader.py
@@ -3,25 +3,6 @@ from torch.utils.data import DataLoader, random_split
 from training.dataset import GEIDataset
 
 
-class AugmentedSubset:
-    def __init__(self, subset, augment: bool = True):
-        self.subset = subset
-        self.dataset = subset.dataset
-        self.indices = subset.indices
-        self.augment = augment
-
-    def __len__(self):
-        return len(self.subset)
-
-    def __getitem__(self, idx):
-        orig_augment = self.dataset.augment
-        self.dataset.augment = self.augment
-        try:
-            return self.dataset[self.indices[idx]]
-        finally:
-            self.dataset.augment = orig_augment
-
-
 def build_dataloaders(
     root_dir: str = "data/casia_processed/gei",
     batch_size: int = 16,
@@ -38,13 +19,10 @@ def build_dataloaders(
     train_size = int(len(dataset) * train_ratio)
     val_size = len(dataset) - train_size
 
-    train_sub, val_sub = random_split(
+    train_dataset, val_dataset = random_split(
         dataset,
         [train_size, val_size],
     )
-
-    train_dataset = AugmentedSubset(train_sub, augment=True)
-    val_dataset = AugmentedSubset(val_sub, augment=False)
 
     train_loader = DataLoader(
         train_dataset,
@@ -60,4 +38,5 @@ def build_dataloaders(
         num_workers=0,
     )
 
-    return train_loader, val_loader, dataset
+    return train_loader, val_loader, dataset
+
